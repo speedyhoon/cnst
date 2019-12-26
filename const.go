432198b0eafda1ffd43172e083323011648e030f
sh <16303791+speedyhoon@users.noreply.github.com>
2019-12-26 18:11:57 +1100

    Add constants and licence.

---
+++ const.go
@@ -0,0 +1,22 @@
+package cnst
+
+const (
+	ContentType    = "content-type"
+	ContentEncode  = "content-encoding"
+	CacheControl   = "cache-control"
+	Expires        = "expires"
+	Cache          = "cache"
+	NoCache        = "nocache"
+	Gzip           = "gzip"
+	Brotli         = "br"
+	AcceptEncoding = "accept-encoding"
+	CSP            = "content-security-policy"
+	XFrameOptions  = "x-frame-options"
+	Deny           = "deny"
+	Vary           = "vary"
+	MimeHTML       = "text/html; charset=utf-8"
+	MimeCSS        = "text/css; charset=utf-8"
+	MimeJS         = "text/javascript"
+	MimeSVG        = "image/svg+xml"
+	MimeWEBP       = "image/webp"
+)
