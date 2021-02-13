f9afd87b05198ee27e4c2633d7c91ce8b85bfa11
sh <16303791+speedyhoon@users.noreply.github.com>
2021-02-13 13:12:54 +1100

    Lint comments

--- const.go
+++ const.go
@@ -2,6 +2,7 @@ package cnst
 
 const (
 	//HTTP Methods developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
 	Connect = "CONNECT"
 	Delete  = "DELETE"
 	Get     = "GET"
@@ -13,6 +14,7 @@ const (
 	Trace   = "TRACE"
 
 	//HTTP Headers developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+
 	AcceptCHLifetime              = "accept-ch-lifetime"
 	AcceptCH                      = "accept-ch"
 	AcceptCharset                 = "accept-charset"
@@ -123,20 +125,24 @@ const (
 	XForwardedProto         = "x-forwarded-proto"*/
 
 	//Accept-Encoding Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding#directives
+
 	Brotli   = "br"
 	Compress = "compress" //Lempel-Ziv-Welch (LZW) algorithm
 	Deflate  = "deflate"  //zlib structure, with the deflate compression algorithm
 	Gzip     = "gzip"     //Lempel-Ziv coding (LZ77), with a 32-bit CRC
 
 	//CacheControl Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#directives
+
 	NoCache = "no-cache"
 	NoStore = "no-store"
 
 	//X-Frame-Options Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options#directives
+
 	Deny = "deny"
 
 	charset = "; charset=utf8"
 	//Common MIME Types developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+
 	Mime3GPP   = "audio/3gpp"
 	Mime3GPPV  = "video/3gpp"
 	Mime3GPP2  = "audio/3gpp2"
