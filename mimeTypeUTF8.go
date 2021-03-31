cd94b199de0096071453742db25364957d025e02
sh <16303791+speedyhoon@users.noreply.github.com>
2021-03-31 20:34:11 +1100

    Move charset & headers into a separate file.

---
+++ mimeTypeUTF8.go
@@ -0,0 +1,10 @@
+package cnst
+
+// Common MIME Types with charset UTF8 set
+const (
+	charset = "; charset=utf8"
+
+	MimeCSSUTF8  = MimeCSS + charset
+	MimeHTMUTF8  = MimeHTM + charset
+	MimeHTMLUTF8 = MimeHTM + charset
+)
