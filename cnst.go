531215001af2e1d41952738483a84b8c29cfcc01
sh <16303791+speedyhoon@users.noreply.github.com>
2021-11-13 14:22:07 +1100

    Add all X-Frame-Options

--- cnst.go
+++ cnst.go
@@ -1,16 +1,21 @@
 package cnst
 
+// Accept-Encoding Directives https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding#directives
 const (
-	//Accept-Encoding Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding#directives
 	Brotli   = "br"
-	Compress = "compress" //Lempel-Ziv-Welch (LZW) algorithm
-	Deflate  = "deflate"  //zlib structure, with the deflate compression algorithm
-	Gzip     = "gzip"     //Lempel-Ziv coding (LZ77), with a 32-bit CRC
+	Compress = "compress" // Lempel-Ziv-Welch (LZW) algorithm.
+	Deflate  = "deflate"  // zlib structure, with the deflate compression algorithm.
+	Gzip     = "gzip"     // Lempel-Ziv coding (LZ77), with a 32-bit CRC.
+)
 
-	//CacheControl Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#directives
+// CacheControl Directives https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#directives
+const (
 	NoCache = "no-cache"
 	NoStore = "no-store"
+)
 
-	//X-Frame-Options Directives developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options#directives
-	Deny = "deny"
+// X-Frame-Options Directives https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options#directives
+const (
+	Deny       = "deny"
+	SameOrigin = "SAMEORIGIN"
 )
