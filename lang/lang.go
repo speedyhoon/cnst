40384afa69481b78e701f3ae02360b4233688034
sh <16303791+speedyhoon@users.noreply.github.com>
2023-11-01 16:19:34 +1100

    Add a short list of languages.

---
+++ lang/lang.go
@@ -0,0 +1,310 @@
+package lang
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang
+// https://gist.github.com/JamieMason/3748498
+const (
+	Afrikaans            = "af-ZA"
+	AfrikaansSouthAfrica = "af-ZA"
+
+	Arabic            = "ar"
+	ArabicUAE         = "ar-AE"
+	ArabicBahrain     = "ar-BH"
+	ArabicAlgeria     = "ar-DZ"
+	ArabicEgypt       = "ar-EG"
+	ArabicIraq        = "ar-IQ"
+	ArabicJordan      = "ar-JO"
+	ArabicKuwait      = "ar-KW"
+	ArabicLebanon     = "ar-LB"
+	ArabicLibya       = "ar-LY"
+	ArabicMorocco     = "ar-MA"
+	ArabicOman        = "ar-OM"
+	ArabicQatar       = "ar-QA"
+	ArabicSaudiArabia = "ar-SA"
+	ArabicSyria       = "ar-SY"
+	ArabicTunisia     = "ar-TN"
+	ArabicYemen       = "ar-YE"
+
+	Azeri                   = "az"
+	AzeriCyrillicAzerbaijan = "az-Cyrl-AZ"
+	AzeriLatinAzerbaijan    = "az-Latn-AZ"
+
+	Belarusian        = "be"
+	BelarusianBelarus = "be-BY"
+
+	Bulgarian         = "bg"
+	BulgarianBulgaria = "bg-BG"
+
+	BosnianBosniaHerzegovina = "bs-Latn-BA"
+
+	Catalan        = "ca"
+	CatalanCatalan = "ca-ES"
+
+	Czech              = "cs"
+	CzechCzechRepublic = "cs-CZ"
+
+	WelshUnitedKingdom = "cy-GB"
+
+	Danish        = "da"
+	DanishDenmark = "da-DK"
+
+	German              = "de"
+	GermanAustria       = "de-AT"
+	GermanGermany       = "de-DE"
+	GermanSwitzerland   = "de-CH"
+	GermanLiechtenstein = "de-LI"
+	GermanLuxembourg    = "de-LU"
+
+	Divehi         = "dv"
+	DivehiMaldives = "dv-MV"
+
+	Greek       = "el"
+	GreekGreece = "el-GR"
+
+	English               = "en"
+	EnglishCaribbean      = "en-029"
+	EnglishAustralia      = "en-AU"
+	EnglishBelize         = "en-BZ"
+	EnglishCanada         = "en-CA"
+	EnglishUnitedKingdom  = "en-GB"
+	EnglishIreland        = "en-IE"
+	EnglishIndia          = "en-IN"
+	EnglishJamaica        = "en-JM"
+	EnglishNewZealand     = "en-NZ"
+	EnglishNigeria        = "en-NG"
+	EnglishPhilippines    = "en-PH"
+	EnglishTrinidadTobago = "en-TT"
+	EnglishUSA            = "en-US"
+	EnglishSouthAfrica    = "en-ZA"
+	EnglishZimbabwe       = "en-ZW"
+
+	Spanish                  = "es"
+	SpanishArgentina         = "es-AR"
+	SpanishBolivia           = "es-BO"
+	SpanishChile             = "es-CL"
+	SpanishColombia          = "es-CO"
+	SpanishCostaRica         = "es-CR"
+	SpanishDominicanRepublic = "es-DO"
+	SpanishEcuador           = "es-EC"
+	SpanishSpain             = "es-ES"
+	SpanishGuatemala         = "es-GT"
+	SpanishHonduras          = "es-HN"
+	SpanishMexico            = "es-MX"
+	SpanishNicaragua         = "es-NI"
+	SpanishPanama            = "es-PA"
+	SpanishPeru              = "es-PE"
+	SpanishPuertoRico        = "es-PR"
+	SpanishParaguay          = "es-PY"
+	SpanishElSalvador        = "es-SV"
+	SpanishUruguay           = "es-UY"
+	SpanishVenezuela         = "es-VE"
+
+	Estonian        = "et"
+	EstonianEstonia = "et-EE"
+
+	Basque       = "eu"
+	BasqueBasque = "eu-ES"
+
+	Persian     = "fa"
+	PersianIran = "fa-IR"
+
+	Finnish        = "fi"
+	FinnishFinland = "fi-FI"
+
+	Faroese             = "fo"
+	FaroeseFaroeIslands = "fo-FO"
+
+	French            = "fr"
+	FrenchBelgium     = "fr-BE"
+	FrenchCanada      = "fr-CA"
+	FrenchFrance      = "fr-FR"
+	FrenchSwitzerland = "fr-CH"
+	FrenchLuxembourg  = "fr-LU"
+	FrenchMonaco      = "fr-MC"
+
+	Galician         = "gl"
+	GalicianGalician = "gl-ES"
+
+	Gujarati      = "gu"
+	GujaratiIndia = "gu-IN"
+
+	Hebrew       = "he"
+	HebrewIsrael = "he-IL"
+
+	Hindi      = "hi"
+	HindiIndia = "hi-IN"
+
+	Croatian                  = "hr"
+	CroatianBosniaHerzegovina = "hr-BA"
+	CroatianCroatia           = "hr-HR"
+
+	Hungarian        = "hu"
+	HungarianHungary = "hu-HU"
+
+	Armenian        = "hy"
+	ArmenianArmenia = "hy-AM"
+
+	Indonesian          = "id"
+	IndonesianIndonesia = "id-ID"
+
+	Icelandic        = "is"
+	IcelandicIceland = "is-IS"
+
+	Italian            = "it"
+	ItalianSwitzerland = "it-CH"
+	ItalianItaly       = "it-IT"
+
+	Japanese      = "ja"
+	JapaneseJapan = "ja-JP"
+
+	Georgian        = "ka"
+	GeorgianGeorgia = "ka-GE"
+
+	Kazakh           = "kk"
+	KazakhKazakhstan = "kk-KZ"
+
+	Kannada      = "kn"
+	KannadaIndia = "kn-IN"
+
+	Korean      = "ko"
+	KoreanKorea = "ko-KR"
+
+	Konkani      = "kok"
+	KonkaniIndia = "kok-IN"
+
+	Kyrgyz           = "ky"
+	KyrgyzKyrgyzstan = "ky-KG"
+
+	Lithuanian          = "lt"
+	LithuanianLithuania = "lt-LT"
+
+	Latvian         = "lv"
+	LatvianLatvia   = "lv-LV"
+	MaoriNewZealand = "mi-NZ"
+
+	Macedonian                                = "mk"
+	MacedonianFormerYugoslavRepublicMacedonia = "mk-MK"
+
+	Mongolian                 = "mn"
+	MongolianCyrillicMongolia = "mn-MN"
+
+	Marathi      = "mr"
+	MarathiIndia = "mr-IN"
+
+	Malay                 = "ms"
+	MalayBruneiDarussalam = "ms-BN"
+	MalayMalaysia         = "ms-MY"
+
+	MalteseMalta = "mt-MT"
+
+	NorwegianNorway = "nb-NO" // Bokmal
+
+	Dutch            = "nl"
+	DutchBelgium     = "nl-BE"
+	DutchNetherlands = "nl-NL"
+
+	//Norwegian, Nynorsk (Norway) = "nn-NO       nn-NO"
+
+	Norwegian = "no          nb-NO"
+
+	NorthernSothoSouthAfrica = "ns-ZA"
+
+	Punjabi      = "pa"
+	PunjabiIndia = "pa-IN"
+
+	Polish       = "pl"
+	PolishPoland = "pl-PL"
+
+	Portuguese         = "pt"
+	PortugueseBrazil   = "pt-BR"
+	PortuguesePortugal = "pt-PT"
+
+	QuechuaBolivia = "quz-BO"
+	QuechuaEcuador = "quz-EC"
+	QuechuaPeru    = "quz-PE"
+
+	Romanian        = "ro"
+	RomanianRomania = "ro-RO"
+
+	Russian       = "ru"
+	RussianRussia = "ru-RU"
+
+	Sanskrit      = "sa"
+	SanskritIndia = "sa-IN"
+
+	SamiNorthernFinland = "se-FI"
+	SamiNorthernNorway  = "se-NO"
+	SamiNorthernSweden  = "se-SE"
+
+	Slovak            = "sk"
+	SlovakSlovakia    = "sk-SK"
+	Slovenian         = "sl"
+	SlovenianSlovenia = "sl-SI"
+
+	SamiSouthernNorway = "sma-NO"
+	SamiSouthernSweden = "sma-SE"
+	SamiLuleNorway     = "smj-NO"
+	SamiLuleSweden     = "smj-SE"
+	SamiInariFinland   = "smn-FI"
+	SamiSkoltFinland   = "sms-FI"
+
+	Albanian        = "sq"
+	AlbanianAlbania = "sq-AL"
+
+	Serbian                          = "sr"
+	SerbianCyrillicBosniaHerzegovina = "sr-Cyrl-BA"
+	SerbianCyrillicSerbia            = "sr-Cyrl-CS"
+	SerbianLatinBosniaHerzegovina    = "sr-Latn-BA"
+	SerbianLatinSerbia               = "sr-Latn-CS"
+
+	Swedish        = "sv"
+	SwedishFinland = "sv-FI"
+	SwedishSweden  = "sv-SE"
+
+	Kiswahili      = "sw"
+	KiswahiliKenya = "sw-KE"
+
+	Syriac      = "syr"
+	SyriacSyria = "syr-SY"
+
+	Tamil      = "ta"
+	TamilIndia = "ta-IN"
+
+	Telugu      = "te"
+	TeluguIndia = "te-IN"
+
+	Thai         = "th"
+	ThaiThailand = "th-Th"
+
+	TswanaSouthAfrica = "tn-ZA"
+
+	Turkish       = "tr"
+	TurkishTurkey = "tr-TR"
+
+	Tatar       = "tt"
+	TatarRussia = "tt-RU"
+
+	Ukrainian        = "uk"
+	UkrainianUkraine = "uk-UA"
+
+	Urdu                        = "ur"
+	UrduIslamicRepublicPakistan = "ur-PK"
+
+	Uzbek                   = "uz"
+	UzbekCyrillicUzbekistan = "uz-Cyrl-UZ"
+	UzbekLatinUzbekistan    = "uz-Latn-UZ"
+
+	Vietnamese        = "vi"
+	VietnameseVietnam = "vi-VN"
+
+	XhosaSouthAfrica = "xh-ZA"
+
+	ChinesePeoplesRepublicChina = "zh-CN"
+	ChineseHongKongSAR          = "zh-HK"
+	ChineseSimplified           = "zh-CHS"
+	ChineseTraditional          = "zh-CHT"
+	ChineseMacaoSAR             = "zh-MO"
+	ChineseSingapore            = "zh-SG"
+	ChineseTaiwan               = "zh-TW"
+
+	ZuluSouthAfrica = "zu-ZA"
+)
