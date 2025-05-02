20575b9123ba1c5acbcd3b28bb529d81808c413b
sh <16303791+speedyhoon@users.noreply.github.com>
2025-05-03 01:36:41 +1000

    Add Zstandard to accept-encoding definitions.

--- cnst.go
+++ cnst.go
@@ -5,10 +5,11 @@ const (
 	Brotli = "br"
 	// Compress Lempel-Ziv-Welch (LZW) algorithm.
 	Compress = "compress"
-	// Deflate zlib structure, with the deflate compression algorithm.
+	// Deflate zlib structure, with the Deflate compression algorithm.
 	Deflate = "deflate"
 	// Gzip Lempel-Ziv coding (LZ77), with a 32-bit CRC.
-	Gzip = "gzip"
+	Gzip      = "gzip"
+	Zstandard = "zstd"
 )
 
 // CacheControl Directives https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#directives
